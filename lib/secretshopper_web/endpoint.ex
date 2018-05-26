06fee362855ec1732bf3605a26b7e5e45896a510
Travis Jefferson <traviscjefferson@gmail.com>
2018-05-26 16:10:48 -0500

    Run create-react-app and serve it from /

--- lib/secretshopper_web/endpoint.ex
+++ lib/secretshopper_web/endpoint.ex
@@ -8,8 +8,9 @@ defmodule SecretshopperWeb.Endpoint do
   # You should set gzip to true if you are running phoenix.digest
   # when deploying your static files in production.
   plug Plug.Static,
-    at: "/", from: :secretshopper, gzip: false,
-    only: ~w(css fonts images js favicon.ico robots.txt)
+    at: "/",
+    from: "priv/clientjs/build/",
+    only: ~w(index.html favicon.ico static)
 
   # Code reloading can be explicitly enabled under the
   # :code_reloader configuration of your endpoint.
