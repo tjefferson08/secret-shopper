70ce6c78cd878b440df3030b0a86da8def94eaa3
Adam Coulter <coulterad@gmail.com>
2018-06-12 08:40:13 -0500

    make that navbar component smarter. need tests

--- priv/clientjs/src/NavBar/NavBar.jsx
+++ priv/clientjs/src/NavBar/NavBar.jsx
@@ -1,21 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 import './NavBar.css';
 
-const NavBar = () =>
-  <ul className="NavBar">
-    <li>
-      <Link to="/dashboard">Dashboard</Link>
-    </li>
-    <li>
-      <Link to="/sign_up">Sign Up</Link>
-    </li>
-    <li>
-      <Link to="/sign_in">Sign In</Link>
-    </li>
-    <li>
-      <Link to="/sign_out">Sign Out</Link>
-    </li>
-  </ul>;
+class NavBar extends Component {
+  constructor(props) {
+    super(props);
 
-export default NavBar;
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <ul className="NavBar">
+        {this.dashboardLink()}
+        {this.signUpLink()}
+        {this.signInLink()}
+        {this.signOutLink()}
+      </ul>
+    );
+  }
+
+  dashboardLink() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+    return (
+      <li>
+        <Link to="/dashboard">Dashboard</Link>
+      </li>
+    );
+  }
+  signUpLink() {
+    if (this.props.isAuthenticated) {
+      return;
+    }
+
+    return (
+      <li>
+        <Link to="/sign_up">Sign Up</Link>
+      </li>
+    );
+  }
+
+  signInLink() {
+    if (this.props.isAuthenticated) {
+      return;
+    }
+
+    return (
+      <li>
+        <Link to="/sign_in">Sign In</Link>
+      </li>
+    );
+  }
+
+  signOutLink() {
+    if (!this.props.isAuthenticated) {
+      return;
+    }
+
+    return (
+      <li>
+        <Link to="/sign_out">Sign Out</Link>
+      </li>
+    );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.authentication.isAuthenticated
+  };
+};
+
+export default connect(mapStateToProps)(NavBar);
