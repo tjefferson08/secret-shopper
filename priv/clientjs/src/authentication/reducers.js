ef377cd7b53bd4bf091e71c70d714041d3aed34f
Travis Jefferson <traviscjefferson@gmail.com>
2018-06-12 20:48:16 -0500

    Handle logout reducer changes

--- priv/clientjs/src/authentication/reducers.js
+++ priv/clientjs/src/authentication/reducers.js
@@ -1,5 +1,12 @@
 import cookie from 'js-cookie';
-import { LOGIN_FAILURE, LOGIN_REQUEST, LOGIN_SUCCESS } from './actions';
+import {
+  LOGIN_FAILURE,
+  LOGIN_REQUEST,
+  LOGIN_SUCCESS,
+  LOGOUT_FAILURE,
+  LOGOUT_REQUEST,
+  LOGOUT_SUCCESS
+} from './actions';
 
 const initialState = () => {
   return {
@@ -12,19 +19,37 @@ const authentication = (state = initialState(), action) => {
   switch (action.type) {
     case LOGIN_REQUEST:
       return {
-        isAuthenticated: false,
-        isFetching: true
+        isFetching: true,
+        ...state
       };
     case LOGIN_SUCCESS:
       return {
         isAuthenticated: true,
         isFetching: false,
-        currentUser: action.user
+        currentUser: action.user,
+        ...state
       };
     case LOGIN_FAILURE:
       return {
         isAuthenticated: false,
-        isFetching: false
+        isFetching: false,
+        ...state
+      };
+    case LOGOUT_REQUEST:
+      return {
+        isFetching: true,
+        ...state
+      };
+    case LOGOUT_SUCCESS:
+      return {
+        isFetching: false,
+        isAuthenticated: false,
+        ...state
+      };
+    case LOGOUT_FAILURE:
+      return {
+        isFetching: false,
+        ...state
       };
     default:
       return state;
