a648e667c82005b88db4232f597064e4df0e21c8
Adam Coulter <coulterad@gmail.com>
2018-06-14 09:51:29 -0500

    tests! with enzyme! prop types too!

--- priv/clientjs/src/NavBar.test.jsx
+++ priv/clientjs/src/NavBar.test.jsx
@@ -1,4 +1,34 @@
 import React from 'react';
+import { shallow } from 'enzyme';
 import NavBar from './NavBar';
+import configureMockStore from 'redux-mock-store';
 
-test('tests', () => {});
+let store;
+
+describe('when authenticated', () => {
+  beforeEach(() => {
+    store = configureMockStore([])({
+      authentication: { isAuthenticated: true }
+    });
+  });
+
+  test('it should render LoggedInLinks', () => {
+    const navBar = shallow(<NavBar store={store} />);
+    expect(navBar.dive().text()).toContain('LoggedInLinks');
+    expect(navBar.dive().text()).not.toContain('LoggedOutLinks');
+  });
+});
+
+describe('when not authenticated', () => {
+  beforeEach(() => {
+    store = configureMockStore([])({
+      authentication: { isAuthenticated: false }
+    });
+  });
+
+  test('it should render LoggedOutLinks', () => {
+    const navBar = shallow(<NavBar store={store} />);
+    expect(navBar.dive().text()).not.toContain('LoggedInLinks');
+    expect(navBar.dive().text()).toContain('LoggedOutLinks');
+  });
+});
