f7a306ac0145239beee167dd8f2f4ddd203f58be
Travis Jefferson <traviscjefferson@gmail.com>
2018-06-24 16:36:37 -0500

    Add recipes list component to render on dashboard
    And some cheap snapshot specs (shallow and deep both IDK what I'm
    doing)

---
+++ priv/clientjs/src/RecipeList/RecipeCard.jsx
@@ -0,0 +1,79 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const RecipeCard = ({ recipe }) => {
+  const {
+    cook_time,
+    prep_time,
+    total_time,
+    ingredients,
+    instructions,
+    name
+  } = recipe;
+  return (
+    <div>
+      <div>
+        <h2>{name || 'Unknown name'}</h2>
+      </div>
+      <Time label="Cook time" value={cook_time} />
+      <Time label="Prep time" value={prep_time} />
+      <Time label="Total time" value={total_time} />
+      <IngredientsList ingredients={ingredients} />
+      <InstructionsList instructions={instructions} />
+    </div>
+  );
+};
+
+const Time = ({ label, value }) => (
+  <div>
+    <strong>{label}</strong>
+    <p>{value}</p>
+  </div>
+);
+
+const IngredientsList = ({ ingredients }) => (
+  <Fragment>
+    <h3>Ingredients</h3>
+    <ul>{ingredients.map(ing => <li key={ing.id}>{ing.name}</li>)}</ul>
+  </Fragment>
+);
+
+const InstructionsList = ({ instructions }) => (
+  <Fragment>
+    <h3>Instructions</h3>
+    <ol>{instructions.map(inst => <li key={inst.id}>{inst.text}</li>)}</ol>
+  </Fragment>
+);
+
+RecipeCard.propTypes = {
+  recipe: PropTypes.shape({
+    id: PropTypes.number,
+    name: PropTypes.string,
+    cook_time: PropTypes.string.isRequired,
+    prep_time: PropTypes.string.isRequired,
+    total_time: PropTypes.string.isRequired,
+    ingredients: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.number,
+        name: PropTypes.string
+      })
+    ).isRequired,
+    instructions: PropTypes.arrayOf(
+      PropTypes.shape({
+        id: PropTypes.string,
+        text: PropTypes.string
+      })
+    ).isRequired
+  }).isRequired
+};
+
+RecipeCard.defaultProps = {
+  recipe: {
+    id: null,
+    name: 'Unnamed Recipe',
+    ingredients: [],
+    instruction: []
+  }
+};
+
+export default RecipeCard;
--- priv/clientjs/src/recipes/RecipeSet.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import RecipeCard from './RecipeCard';
-
-const RecipeSet = ({ recipes = [] }) =>
-  <div>
-    {recipes.map(recipe => <RecipeCard recipe={recipe} key={recipe.id} />)}
-  </div>;
-
-export default RecipeSet;
