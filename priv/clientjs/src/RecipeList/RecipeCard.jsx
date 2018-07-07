67c62014c67ad61f2230dfeee522b224d4116cf7
Travis Jefferson <traviscjefferson@gmail.com>
2018-07-07 14:19:05 -0500

    Try out pure.css (grid only for the time being)
    And some super basic really poorly written CSS

--- priv/clientjs/src/RecipeList/RecipeCard.jsx
+++ priv/clientjs/src/RecipeList/RecipeCard.jsx
@@ -1,7 +1,7 @@
 import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 
-const RecipeCard = ({ recipe }) => {
+const RecipeCard = ({ recipe, showDetails }) => {
   const {
     cook_time,
     image_url,
@@ -12,37 +12,44 @@ const RecipeCard = ({ recipe }) => {
     total_time
   } = recipe;
   return (
-    <div>
-      <div>
-        <h2>{name || 'Unknown name'}</h2>
+    <div className="pure-u-1 pure-u-md-1-3">
+      <div className="recipe-card">
+        <div className="header">
+          <i className="far fa-star fa-2x" />
+          <h3 className="name">{name}</h3>
+        </div>
+        <img className="thumbnail" src={image_url} />
+        <Time label="Cook time" value={cook_time} />
+        <Time label="Prep time" value={prep_time} />
+        <Time label="Total time" value={total_time} />
+        {showDetails && (
+          <Fragment>
+            <IngredientsList ingredients={ingredients} />
+            <InstructionsList instructions={instructions} />
+          </Fragment>
+        )}
       </div>
-      <img className="thumbnail" src={image_url} />
-      <Time label="Cook time" value={cook_time} />
-      <Time label="Prep time" value={prep_time} />
-      <Time label="Total time" value={total_time} />
-      <IngredientsList ingredients={ingredients} />
-      <InstructionsList instructions={instructions} />
     </div>
   );
 };
 
 const Time = ({ label, value }) => (
-  <div>
-    <strong>{label}</strong>
-    <p>{value}</p>
+  <div className="time-container">
+    <strong className="time-label">{label}:</strong>
+    <p className="time-value">{value}</p>
   </div>
 );
 
 const IngredientsList = ({ ingredients }) => (
   <Fragment>
-    <h3>Ingredients</h3>
+    <h4>Ingredients</h4>
     <ul>{ingredients.map(ing => <li key={ing.id}>{ing.name}</li>)}</ul>
   </Fragment>
 );
 
 const InstructionsList = ({ instructions }) => (
   <Fragment>
-    <h3>Instructions</h3>
+    <h4>Instructions</h4>
     <ol>{instructions.map(inst => <li key={inst.id}>{inst.text}</li>)}</ol>
   </Fragment>
 );
--- priv/clientjs/src/RecipeList/RecipeList.jsx
+++ priv/clientjs/src/RecipeList/RecipeList.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import RecipeCard from './RecipeCard';
+import './RecipeList.css';
 
 const RecipeList = props => {
   const { recipes = [] } = props;
   return (
-    <div>
+    <div className="recipe-list pure-g">
       {recipes.map(recipe => <RecipeCard recipe={recipe} key={recipe.id} />)}
     </div>
   );
